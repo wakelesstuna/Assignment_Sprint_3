5078c8e41264f933cef6ea5ae1e7a6c8e36d821a
PatrikJMelander <patrikjmelander@gmail.com>
2020-10-22 11:33:12 +0200

    made the blank button transparant

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collector;
 
 
 public class GameBoard extends JFrame {
