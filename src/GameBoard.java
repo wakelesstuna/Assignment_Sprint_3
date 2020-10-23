87e52f9d22cebba56e8a20304c4cf94f6665d8e9
PatrikJMelander <patrikjmelander@gmail.com>
2020-10-23 12:09:59 +0200

    created a method that split an image into 16 pieces

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -34,7 +34,8 @@ public class GameBoard extends JFrame {
     JButton newGameButton = new JButton("New Game");
     JButton cheatButton = new JButton("Cheat");
 
-    public GameBoard(String playerName) {
+    public GameBoard(String playerName) throws IOException {
+        addPictureToButton();
         //u.loadGameMusic(musicPath); // comment out to kill music
         parent.setLayout(new FlowLayout());
 
@@ -45,11 +46,11 @@ public class GameBoard extends JFrame {
 
         title.setBackground(Color.GREEN);
         title.add(labelTitle);
-        gameBoard.setPreferredSize(new Dimension(500,500));
+        gameBoard.setPreferredSize(new Dimension(500, 500));
         labelTitle.setFont(new Font("Georgia", Font.BOLD, 32));
 
         newGameButton.addActionListener(e -> newGame());
-        cheatButton.addActionListener(e ->cheatButton());
+        cheatButton.addActionListener(e -> cheatButton());
 
         bottomPanel.add(playerNameLabel = new JLabel("Player: " + playerName));
         bottomPanel.add(gameTime);
@@ -98,14 +99,14 @@ public class GameBoard extends JFrame {
         Collections.shuffle(list);
     }
 
-    public void cheatButton(){
+    public void cheatButton() {
         u.gameTimerStop();
         gameBoard.removeAll();
         buttonsList.clear();
         renderButtons(winCondition);
     }
 
-    public void newGame(){
+    public void newGame() {
         gameOver = false;
         gameBoard.removeAll();
         buttonsList.clear();
@@ -122,13 +123,13 @@ public class GameBoard extends JFrame {
         u.gameTimer(gameTime);
     }
 
-    public void winScreen(){
+    public void winScreen() {
 
         u.gameTimerStop();
         String gameTimeComplete = gameTime.getText();
         JTextArea test = new JTextArea("were");
         JFrame winFrame = new JFrame();
-        JPanel winPanel = new JPanel(new GridLayout(3,1));
+        JPanel winPanel = new JPanel(new GridLayout(3, 1));
         JLabel winLabel = new JLabel("WINNER WINNER", SwingConstants.CENTER);
         JLabel timeLabel = new JLabel(gameTimeComplete, SwingConstants.CENTER);
         JLabel playerNameLabel = new JLabel(playerName, SwingConstants.CENTER);
@@ -138,8 +139,8 @@ public class GameBoard extends JFrame {
         winPanel.add(timeLabel);
         winFrame.add(test);
 
-        winFrame.setLocation(600,200);
-        winFrame.setSize(300,300);
+        winFrame.setLocation(600, 200);
+        winFrame.setSize(300, 300);
         winFrame.setVisible(true);
     }
 
@@ -164,13 +165,13 @@ public class GameBoard extends JFrame {
         return emptyButtonIndex;
     }
 
-    public void turnAllButtonsGreenWhenWin(){
+    public void turnAllButtonsGreenWhenWin() {
         for (Button button : buttonsList) {
             button.getButton().setBackground(Color.GREEN);
         }
     }
 
-    public boolean isWinCondition(){
+    public boolean isWinCondition() {
         for (int i = 0; i < winCondition.size(); i++) {
             int tempWin = winCondition.get(i).getButtonID();
             int tempActual = buttonsList.get(i).getButtonID();
@@ -181,12 +182,12 @@ public class GameBoard extends JFrame {
     }
 
     public void moveButton(int index) {
-        if(!gameOver) {
+        if (!gameOver) {
             Collections.swap(buttonsList, index, findEmptyButton());
             renderButtons(buttonsList);
             clickCounter.setText("Antal klick: " + u.counter++);
             gameBoard.updateUI();
-            if(gameOver = isWinCondition()){
+            if (gameOver = isWinCondition()) {
                 turnAllButtonsGreenWhenWin();
                 winScreen();
             }
@@ -198,109 +199,174 @@ public class GameBoard extends JFrame {
 
     public void checkIfClickedButtonIsNextToEmptyButton(Button clickedButton) {
         switch (buttonsList.indexOf(clickedButton)) {
-            case 0 -> { if (buttonsList.get(1).equals(buttonsList.get(findEmptyButton())))
-                moveButton(0);
-            else if (buttonsList.get(4).equals(buttonsList.get(findEmptyButton())))
-                moveButton(0); }
-            case 1 -> { if (buttonsList.get(0).equals(buttonsList.get(findEmptyButton())))
-                moveButton(1);
-            else if (buttonsList.get(2).equals(buttonsList.get(findEmptyButton())))
-                moveButton(1);
-            else if (buttonsList.get(5).equals(buttonsList.get(findEmptyButton())))
-                moveButton(1); }
-            case 2 -> { if (buttonsList.get(1).equals(buttonsList.get(findEmptyButton())))
-                moveButton(2);
-            else if (buttonsList.get(3).equals(buttonsList.get(findEmptyButton())))
-                moveButton(2);
-            else if (buttonsList.get(6).equals(buttonsList.get(findEmptyButton())))
-                moveButton(2); }
-            case 3 -> { if (buttonsList.get(2).equals(buttonsList.get(findEmptyButton())))
-                moveButton(3);
-            else if (buttonsList.get(7).equals(buttonsList.get(findEmptyButton())))
-                moveButton(3); }
-            case 4 -> { if (buttonsList.get(0).equals(buttonsList.get(findEmptyButton())))
-                moveButton(4);
-            else if (buttonsList.get(5).equals(buttonsList.get(findEmptyButton())))
-                moveButton(4);
-            else if (buttonsList.get(8).equals(buttonsList.get(findEmptyButton())))
-                moveButton(4); }
-            case 5 -> { if (buttonsList.get(1).equals(buttonsList.get(findEmptyButton())))
-                moveButton(5);
-            else if (buttonsList.get(6).equals(buttonsList.get(findEmptyButton())))
-                moveButton(5);
-            else if (buttonsList.get(4).equals(buttonsList.get(findEmptyButton())))
-                moveButton(5);
-            else if (buttonsList.get(9).equals(buttonsList.get(findEmptyButton())))
-                moveButton(5); }
-            case 6 -> { if (buttonsList.get(2).equals(buttonsList.get(findEmptyButton())))
-                moveButton(6);
-            else if (buttonsList.get(7).equals(buttonsList.get(findEmptyButton())))
-                moveButton(6);
-            else if (buttonsList.get(10).equals(buttonsList.get(findEmptyButton())))
-                moveButton(6);
-            else if (buttonsList.get(5).equals(buttonsList.get(findEmptyButton())))
-                moveButton(6); }
-            case 7 -> { if (buttonsList.get(3).equals(buttonsList.get(findEmptyButton())))
-                moveButton(7);
-            else if (buttonsList.get(6).equals(buttonsList.get(findEmptyButton())))
-                moveButton(7);
-            else if (buttonsList.get(11).equals(buttonsList.get(findEmptyButton())))
-                moveButton(7); }
-            case 8 -> { if (buttonsList.get(12).equals(buttonsList.get(findEmptyButton())))
-                moveButton(8);
-            else if (buttonsList.get(4).equals(buttonsList.get(findEmptyButton())))
-                moveButton(8);
-            else if (buttonsList.get(9).equals(buttonsList.get(findEmptyButton())))
-                moveButton(8); }
-            case 9 -> { if (buttonsList.get(5).equals(buttonsList.get(findEmptyButton())))
-                moveButton(9);
-            else if (buttonsList.get(10).equals(buttonsList.get(findEmptyButton())))
-                moveButton(9);
-            else if (buttonsList.get(8).equals(buttonsList.get(findEmptyButton())))
-                moveButton(9);
-            else if (buttonsList.get(13).equals(buttonsList.get(findEmptyButton())))
-                moveButton(9); }
-            case 10 -> { if (buttonsList.get(6).equals(buttonsList.get(findEmptyButton())))
-                moveButton(10);
-            else if (buttonsList.get(11).equals(buttonsList.get(findEmptyButton())))
-                moveButton(10);
-            else if (buttonsList.get(9).equals(buttonsList.get(findEmptyButton())))
-                moveButton(10);
-            else if (buttonsList.get(14).equals(buttonsList.get(findEmptyButton())))
-                moveButton(10); }
-            case 11 -> { if (buttonsList.get(7).equals(buttonsList.get(findEmptyButton())))
-                moveButton(11);
-            else if (buttonsList.get(10).equals(buttonsList.get(findEmptyButton())))
-                moveButton(11);
-            else if (buttonsList.get(15).equals(buttonsList.get(findEmptyButton())))
-                moveButton(11); }
-            case 12 -> { if (buttonsList.get(8).equals(buttonsList.get(findEmptyButton())))
-                moveButton(12);
-            else if (buttonsList.get(13).equals(buttonsList.get(findEmptyButton())))
-                moveButton(12); }
-            case 13 -> { if (buttonsList.get(12).equals(buttonsList.get(findEmptyButton())))
-                moveButton(13);
-            else if (buttonsList.get(9).equals(buttonsList.get(findEmptyButton())))
-                moveButton(13);
-            else if (buttonsList.get(14).equals(buttonsList.get(findEmptyButton())))
-                moveButton(13); }
-            case 14 -> { if (buttonsList.get(13).equals(buttonsList.get(findEmptyButton())))
-                moveButton(14);
-            else if (buttonsList.get(10).equals(buttonsList.get(findEmptyButton())))
-                moveButton(14);
-            else if (buttonsList.get(15).equals(buttonsList.get(findEmptyButton())))
-                moveButton(14); }
-            case 15 -> { if (buttonsList.get(11).equals(buttonsList.get(findEmptyButton())))
-                moveButton(15);
-            else if (buttonsList.get(14).equals(buttonsList.get(findEmptyButton())))
-                moveButton(15); }
+            case 0 -> {
+                if (buttonsList.get(1).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(0);
+                else if (buttonsList.get(4).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(0);
+            }
+            case 1 -> {
+                if (buttonsList.get(0).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(1);
+                else if (buttonsList.get(2).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(1);
+                else if (buttonsList.get(5).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(1);
+            }
+            case 2 -> {
+                if (buttonsList.get(1).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(2);
+                else if (buttonsList.get(3).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(2);
+                else if (buttonsList.get(6).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(2);
+            }
+            case 3 -> {
+                if (buttonsList.get(2).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(3);
+                else if (buttonsList.get(7).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(3);
+            }
+            case 4 -> {
+                if (buttonsList.get(0).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(4);
+                else if (buttonsList.get(5).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(4);
+                else if (buttonsList.get(8).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(4);
+            }
+            case 5 -> {
+                if (buttonsList.get(1).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(5);
+                else if (buttonsList.get(6).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(5);
+                else if (buttonsList.get(4).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(5);
+                else if (buttonsList.get(9).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(5);
+            }
+            case 6 -> {
+                if (buttonsList.get(2).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(6);
+                else if (buttonsList.get(7).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(6);
+                else if (buttonsList.get(10).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(6);
+                else if (buttonsList.get(5).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(6);
+            }
+            case 7 -> {
+                if (buttonsList.get(3).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(7);
+                else if (buttonsList.get(6).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(7);
+                else if (buttonsList.get(11).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(7);
+            }
+            case 8 -> {
+                if (buttonsList.get(12).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(8);
+                else if (buttonsList.get(4).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(8);
+                else if (buttonsList.get(9).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(8);
+            }
+            case 9 -> {
+                if (buttonsList.get(5).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(9);
+                else if (buttonsList.get(10).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(9);
+                else if (buttonsList.get(8).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(9);
+                else if (buttonsList.get(13).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(9);
+            }
+            case 10 -> {
+                if (buttonsList.get(6).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(10);
+                else if (buttonsList.get(11).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(10);
+                else if (buttonsList.get(9).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(10);
+                else if (buttonsList.get(14).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(10);
+            }
+            case 11 -> {
+                if (buttonsList.get(7).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(11);
+                else if (buttonsList.get(10).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(11);
+                else if (buttonsList.get(15).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(11);
+            }
+            case 12 -> {
+                if (buttonsList.get(8).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(12);
+                else if (buttonsList.get(13).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(12);
+            }
+            case 13 -> {
+                if (buttonsList.get(12).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(13);
+                else if (buttonsList.get(9).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(13);
+                else if (buttonsList.get(14).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(13);
+            }
+            case 14 -> {
+                if (buttonsList.get(13).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(14);
+                else if (buttonsList.get(10).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(14);
+                else if (buttonsList.get(15).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(14);
+            }
+            case 15 -> {
+                if (buttonsList.get(11).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(15);
+                else if (buttonsList.get(14).equals(buttonsList.get(findEmptyButton())))
+                    moveButton(15);
+            }
         }
     }
-    public void addPictureToButton() throws IOException {
-        final BufferedImage source = ImageIO.read(new File("C:\\Users\\Min Dator\\Desktop\\Java\\Assignment\\OOPJ\\GameOFFifteen\\Pictures\\sunset"));
-        int idx = 0;
-        for (int y = 0; y < source.getHeight(); y += 16) {
-            ImageIO.write(source.getSubimage(0, y, 16, 16), "JPG", new File("C:\\Users\\Min Dator\\Desktop\\Java\\Assignment\\OOPJ\\GameOFFifteen\\Pictures\\sunset" + idx++ + ".JPG"));
+
+    public void addPictureToButton() {
+        int row = 4;
+        int column = 4;
+        try {
+            BufferedImage originalImgage = ImageIO.read(new File("Pictures\\sunset.JPG"));
+
+            int pictureWidth = originalImgage.getWidth();
+            int pictureHeight = originalImgage.getHeight();
+
+            int buttonWidth = pictureWidth / column;
+            int buttonHeight = pictureHeight / row;
+
+            int x = 0;
+            int y = 0;
+
+            for (int i = 0; i < row; i++) {
+                y = 0;
+                for (int j = 0; j < column; j++) {
+                    try {
+                        System.out.println("creating piece: " + i + " " + j);
+
+                        BufferedImage SubImgage = originalImgage.getSubimage(y, x, buttonWidth, buttonHeight);
+                        File outputFile = new File("Pictures\\sunset" + j + i + ".JPG");
+                        ImageIO.write(SubImgage, "jpg", outputFile);
+
+                        y += buttonWidth;
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                x += buttonHeight;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
 
         }
     }
